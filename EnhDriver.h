04ca9b01180383abd388d0c73494f2784fe6ca31
Miguel Rodríguez Pérez <Miguel.Rodriguez@det.uvigo.es>
2015-03-17 17:10:22 +0100

    Optimum beacon rate calculation

--- EnhDriver.h
+++ EnhDriver.h
@@ -18,7 +18,7 @@ class EnhNode;
 class EnhDriver : public PWDriver {
     float max_beacon_rate_; // Max_rate in beacons/s
     float beacon_rate_;
-    
+
     std::queue<Event::evtime_t> next_beacons;
 
     const EnhNode& getEnhNode() const;
@@ -31,10 +31,19 @@ class EnhDriver : public PWDriver {
 
     auto arePendingBeacons(Event::evtime_t now) {
         flushOldBeacons(now);
-        
+
         return next_beacons.empty() != true;
     }
-    
+
+    float calculateOptimumBeaconRate(float data_rate) const noexcept {
+        const auto et = 8 * 5 / Link::getCapacity(); // Beacon transmission time. FIXME: Share a var with Node.h
+        const auto epkt = 8 * Packet::getMaxPacketSize() / Link::getCapacity(); // Packet transmission time
+        const auto eb = 10e-3; // Beacon listening time
+
+        auto optimum_rate = ((2 * eb + 2 * epkt - et - M_E * et) * data_rate) / (eb + epkt + et);
+        return optimum_rate;
+    }
+
 public:
 
     EnhDriver(const EnhNode& node, Event::evtime_t bitlen, float max_rate = 10);
@@ -43,10 +52,12 @@ public:
         return beacon_rate_;
     }
 
-    auto setBeaconRate(float new_rate, Event::evtime_t now) {
+    auto setBeaconRate(float estimated_data_rate, Event::evtime_t now) {
         if (arePendingBeacons(now)) // Only change factor after a whole normal period. This is important so senders do not desynchronize themselves and expend useless time listening
             return beacon_rate_;
-        
+
+        auto new_rate = calculateOptimumBeaconRate(estimated_data_rate);
+
         beacon_rate_ = std::min(new_rate, max_beacon_rate_);
         return beacon_rate_;
     }
@@ -58,17 +69,17 @@ public:
 
         const auto next_beat = now + getTimeUntilListen();
         const auto period_length = next_beat - now;
-        const auto subinterval = 1/(1. * max_beacon_rate_);
+        const auto subinterval = 1 / (1. * max_beacon_rate_);
         /* We generate all possible beacons and then choose randomly which ones to 
          * observe. As the random seed we employ node_id<<16 + floor(now). That is
          * random enough and, at the same time, known by senders */
-        auto rng = PseudoRNG((getNode().getId() << 16) + static_cast<int>(floor(now)));
-        const auto threshold = getBeaconRate()/max_beacon_rate_ * rng.randMax();
-        
+        auto rng = PseudoRNG((getNode().getId() << 16) + static_cast<int> (floor(now)));
+        const auto threshold = getBeaconRate() / max_beacon_rate_ * rng.randMax();
+
         for (auto i = next_beat - period_length + subinterval; i < next_beat; i += subinterval)
             if (rng() <= threshold)
                 next_beacons.push(i);
-        
+
         next_beacons.push(next_beat); // This is the noral beacon that MUST be transmitted                
 
         return scheduleRx(now); // Now there should exist a valid beacon time
